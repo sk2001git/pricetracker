f4ddab4e2dee3890eca11993b01715c7ea7aa07f
sk2001git <122198303+sk2001git@users.noreply.github.com>
2024-01-07 18:18:17 +0800

    Fix build errors

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -7,6 +7,10 @@ import { getAveragePrice, getHighestPrice, getLowestPrice } from "@/lib/utils";
 import { Product as ProductType } from "@/types";
 import { NextResponse } from 'next/server';
 
+export const maxDuration = 300; // 5 minutes
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET() {
   try {
     connectToDB();
@@ -35,7 +39,7 @@ export async function GET() {
         }
 
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: scrapedProduct.url},
+          { url: product.url},
           product,
         );
 
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           alt="logo"
          />
          <p className="nav-logo">
-          Price<span className="text-primary">Wise</span>"
+          Price<span className="text-primary">Wise</span>
          </p>
         </Link>
 
